0f328dec9c112bade82cda65c27ffe79fb2ff924
priyanka1001 <67691762+priyanka1001@users.noreply.github.com>
2020-07-01 17:42:53 +0530

    Add files via upload

---
+++ Animal.java
@@ -0,0 +1,16 @@
+package Inheritance;
+
+public class Animal {
+
+
+		public void sleep() 
+		{
+		    System.out.println("Animal sleeps");
+		}
+		public void eats() 
+		{
+		    System.out.println("Animal eats");
+		}
+	}
+
+ 
---
+++ Person.java
@@ -0,0 +1,26 @@
+package Inheritance;
+
+public class Person {
+
+
+		private String name;
+		public Person(String n)
+		{
+			name=n;
+		}
+		public void setname(String n)
+		{
+			name=n;
+		}
+		public String getName()
+		{
+			return name;
+		}
+		public String toString()
+		{
+			return name;
+		}
+	}
+
+
+ 
